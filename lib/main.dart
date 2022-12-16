7cfe543aae0f6cde5a2da8d3b18fab5aef2a0f70
Lans12345 <olanalans12345@gmail.com>
2022-12-16 18:55:32 +0800

    fixed issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:natcorp/Pages/login/login_page.dart';
+import 'package:natcorp/firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
