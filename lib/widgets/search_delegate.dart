0701834ff243f68f8594a17b87df9098488e7361
Lans12345 <olanalans12345@gmail.com>
2022-12-18 19:45:35 +0800

    initial search delegate

---
+++ lib/widgets/search_delegate.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class MySearchDelegate extends SearchDelegate {
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    // TODO: implement buildActions
+
+    return [
+      IconButton(
+        onPressed: () {
+          if (query != '') {
+            close(context, null);
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                duration: Duration(seconds: 1),
+                content: Text('No Input. Cannot Procceed'),
+              ),
+            );
+          }
+        },
+        icon: const Icon(
+          Icons.check,
+          color: Colors.green,
+        ),
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    return Container();
+  }
+}
