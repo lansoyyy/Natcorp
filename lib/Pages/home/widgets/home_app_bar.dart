42a157ab0e6d5a771e0058852ed4c775062dfe1d
Lans12345 <olanalans12345@gmail.com>
2022-12-19 08:26:41 +0800

    added notif and badges

--- lib/Pages/home/widgets/home_app_bar.dart
+++ lib/Pages/home/widgets/home_app_bar.dart
@@ -1,63 +1,65 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:natcorp/Pages/notif/notif_page.dart';
+import 'package:natcorp/widgets/text_widget.dart';
 
 class HomeAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(
-        top: MediaQuery.of(context).padding.top,
-        left: 25,
-        right: 25,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              Text(
-                'Welcome home',
-                style:
-                    TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Princess Villegas',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
-              )
-            ],
-          ),
-          //Notification
-          Row(
-            children: [
-              GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const NotifPage()));
-                },
-                child: Container(
-                  margin: const EdgeInsets.only(top: 30, right: 25),
-                  transform: Matrix4.rotationZ(100),
-                  child: Stack(
-                    children: const [
-                      Icon(
-                        Icons.notifications_none_outlined,
-                        size: 30,
-                        color: Colors.grey,
-                      ),
-                    ],
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Container(
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top,
+          left: 25,
+          right: 25,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Welcome home',
+                  style: TextStyle(
+                      color: Colors.grey, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Princess Villegas',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+                )
+              ],
+            ),
+
+            //Notification
+            Row(
+              children: [
+                Badge(
+                  badgeContent:
+                      TextRegular(text: '1', fontSize: 12, color: Colors.white),
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const NotifPage()));
+                    },
+                    icon: const Icon(
+                      Icons.notifications_none_outlined,
+                      size: 30,
+                      color: Colors.grey,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-            ],
-          ),
-        ],
+                const SizedBox(
+                  width: 10,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
