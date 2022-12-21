d325ae5a6510a0d30c936c05ffa8ea608b0a8f35
Lans12345 <olanalans12345@gmail.com>
2022-12-21 19:37:27 +0800

    added files screen layout

--- lib/Pages/Files/file_page.dart
+++ lib/Pages/Files/file_page.dart
@@ -1,34 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:natcorp/widgets/text_widget.dart';
 
 class FilesScreen extends StatelessWidget {
-  const FilesScreen({Key? key}) : super(key: key);
+  final filesList = [
+    'NSO',
+    'NBI',
+    'Diploma',
+    'COE',
+    'SSS',
+    'Philhealth',
+    'Pag-ibig',
+    'TIN',
+    'TOR',
+    'Brgy. Clearance',
+    'Police Clearance',
+    'Vaccine Card'
+  ];
 
   @override
   Widget build(BuildContext context) {
     final formKey = GlobalKey<FormState>();
     return Scaffold(
         backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Container(
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(36.0),
-              child: Form(
-                key: formKey,
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Center(
-                        child: SizedBox(
-                          height: 150,
-                          child: Image.asset("assets/logo.png",
-                              fit: BoxFit.contain),
-                        ),
+        body: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Center(
+                      child: SizedBox(
+                        height: 150,
+                        child:
+                            Image.asset("assets/logo.png", fit: BoxFit.contain),
                       ),
-                      const SizedBox(height: 20),
-                    ]),
-              ),
+                    ),
+                    SizedBox(
+                      height: 400,
+                      child: ListView.builder(
+                          itemCount: filesList.length,
+                          itemBuilder: ((context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                              child: SizedBox(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Card(
+                                      elevation: 3,
+                                      child: SizedBox(
+                                          height: 40,
+                                          width: 200,
+                                          child: Center(
+                                            child: TextRegular(
+                                                text: filesList[index],
+                                                fontSize: 18,
+                                                color: Colors.black),
+                                          )),
+                                    ),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    GestureDetector(
+                                      onTap: () {
+                                        print(filesList[index]);
+                                      },
+                                      child: const Card(
+                                        elevation: 3,
+                                        child: SizedBox(
+                                            height: 40,
+                                            width: 50,
+                                            child: Center(
+                                              child: Icon(Icons
+                                                  .add_circle_outline_rounded),
+                                            )),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          })),
+                    ),
+                  ]),
             ),
           ),
         ));
