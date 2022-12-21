8c207e2fa0f404034bdf7fde24af4992a6cd3e6b
Lans12345 <olanalans12345@gmail.com>
2022-12-21 19:21:37 +0800

    wip files screen

--- lib/Pages/Files/file_page.dart
+++ lib/Pages/Files/file_page.dart
@@ -8,50 +8,25 @@ class FilesScreen extends StatelessWidget {
     final formKey = GlobalKey<FormState>();
     return Scaffold(
         backgroundColor: Colors.white,
-        body: Center(
-          child: SingleChildScrollView(
-            child: Container(
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(36.0),
-                child: Form(
-                  key: formKey,
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        SizedBox(
-                          height: 150,
-                          child: Image.asset("assets/logo.png",
-                              fit: BoxFit.contain),
-                        ),
-                        const SizedBox(height: 40),
-                        const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text(
-                            "Coming soon.",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15,
-                                fontWeight: FontWeight.normal),
-                          ),
-                        ),
-                        const SizedBox(height: 15),
-                        const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text(
-                            "File Screen",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ]),
-                ),
-              ),
+        body: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Center(
+                      child: SizedBox(
+                        height: 150,
+                        child:
+                            Image.asset("assets/logo.png", fit: BoxFit.contain),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                  ]),
             ),
           ),
         ));
