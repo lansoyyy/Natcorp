48fc42cc73d8adf8d05c74220990ea255181eb76
Lans12345 <olanalans12345@gmail.com>
2022-12-27 18:59:39 +0800

    added comment repo

---
+++ lib/services/repositories/add_comment.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future addRatings(String name, String profilePicture, String userId,
+    String comment, String companyId) async {
+  final docUser = FirebaseFirestore.instance.collection('Comments').doc();
+
+  final json = {
+    'name': name,
+    'id': docUser.id,
+    'profilePicture': profilePicture,
+    'userId': userId,
+    'dateTime': DateTime.now(),
+    'comment': comment,
+    'companyId': companyId,
+  };
+
+  await docUser.set(json);
+}
