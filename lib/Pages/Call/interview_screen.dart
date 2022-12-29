44d29502cdd50f69f47e88e5f6240ebcf6de476a
Lance_Olana <lance.olana@curaesoft.com>
2022-12-30 07:38:10 +0800

    wip interview screen

---
+++ lib/Pages/Call/interview_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InterviewScreen extends StatelessWidget {
+  const InterviewScreen({Key? key}) : super(key: key);
+
+  // 1bb40b73746f41d181b09b110635de77
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
